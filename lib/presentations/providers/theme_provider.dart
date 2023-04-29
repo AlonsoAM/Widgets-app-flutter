7995d68d756bec214df4706735d1272b145ff5be
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-29 12:23:21 -0500

    Tarea de riverpod finalizada

---
+++ lib/presentations/providers/theme_provider.dart
@@ -0,0 +1,5 @@
+// estado = isDarkMode = boolean
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final isDarkmodeProvider = StateProvider<bool>((ref) => false);
--- lib/presentations/screens/counter/counter_screen.dart
+++ lib/presentations/screens/counter/counter_screen.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentations/providers/counter_provider.dart';
+import 'package:widgets_app/presentations/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   const CounterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkmodeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contador Riverpod'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              },
+              icon: Icon(isDarkMode
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined))
+        ],
       ),
-      body:  Center(
+      body: Center(
         child: Text(
           'Valor: $clickCounter',
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(counterProvider.notifier).state++;
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
