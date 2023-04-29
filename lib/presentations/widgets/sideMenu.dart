18efe57b84897132ddf89b7e70542f2620171454
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-29 11:36:11 -0500

    Implementacion del menú lateral Finalizado

--- lib/presentations/widgets/sideMenu.dart
+++ lib/presentations/widgets/sideMenu.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
-  const SideMenu({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({Key? key, required this.scaffoldKey}) : super(key: key);
 
   @override
   State<SideMenu> createState() => _SideMenuState();
 }
 
 class _SideMenuState extends State<SideMenu> {
-  int navDrawerIndex = 1;
+  int navDrawerIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +24,9 @@ class _SideMenuState extends State<SideMenu> {
           setState(() {
             navDrawerIndex = value;
           });
+          final menuItem = appMenuItems[value];
+          context.push(menuItem.link);
+          widget.scaffoldKey.currentState?.closeDrawer();
         },
         children: [
           Padding(
